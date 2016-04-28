773b9f0e15f891a5a8b9ec289a41cc6c987da083
Donna Peplinskie <donna.peplinskie@risevision.com>
2016-04-28 16:29:01 -0400

    Show data using FixedDataTable

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,92 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
+const React = require("react");
+const ReactDOM = require("react-dom");
+const {Table, Column, Cell} = require("fixed-data-table");
 
-document.addEventListener("DOMContentLoaded", function() {
-  // ReactDOM.render(<Component />, document.getElementById("app"));
+const sheet = document.querySelector("rise-google-sheet");
+
+const FinancialTable = React.createClass({
+  getInitialState: function() {
+    return {
+      data: null
+    };
+  },
+
+  componentDidMount: function() {
+    sheet.addEventListener("rise-google-sheet-response", function(e) {
+      if (e.detail && e.detail.cells) {
+        this.setState({ data: e.detail.cells });
+      }
+    }.bind(this));
+
+    sheet.go();
+  },
+
+  componentWillUnmount: function() {
+    sheet.removeEventListener("rise-google-sheet-response");
+  },
+
+  // Convert data to a two-dimensional array of rows.
+  getRows: function() {
+    var rows = [],
+      row = [];
+
+    for (var i = 0; i < this.state.data.length; i++) {
+      if (this.state.data[i].gs$cell.col === '1') {
+        if (row !== null) {
+          rows.push(row);
+        }
+
+        row = [];
+      }
+
+      row.push(this.state.data[i].content.$t);
+    }
+
+    rows.push(row);
+
+    return rows;
+  },
+
+  render: function() {
+    var totalCols = 6,
+      rows = null,
+      cols = [];
+
+    if (this.state.data) {
+      rows = this.getRows();
+
+      // Create the columns.
+      for (var i = 0; i < totalCols; i++) {
+        cols.push(
+          <Column
+            header={<Cell>{rows[1][i]}</Cell>}
+            cell={ props => (
+              <Cell>
+                {rows[props.rowIndex][i]}
+              </Cell>
+            )}
+            width={200}
+          />
+        );
+      }
+
+      return(
+        <Table
+          rowHeight={50}
+          rowsCount={104} // Need to set this dynamically.
+          width={1200}    // rsW
+          height={800}   // rsH
+          headerHeight={50}>
+          {cols}
+        </Table>
+      );
+    }
+    else {
+      return null;
+    }
+  }
+});
+
+window.addEventListener("WebComponentsReady", function(e) {
+  ReactDOM.render(<FinancialTable />, document.getElementById("app"));
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require("path");
 var webpack = require("webpack");
 var packageData = require("./package.json");
-var filename = [packageData.name, packageData.version, "js"];
+var filename = [packageData.name, "js"];
 
 module.exports = {
   entry: path.resolve(__dirname, packageData.main),
